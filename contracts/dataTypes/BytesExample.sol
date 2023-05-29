26acce351468a865a5cbbf62f8da147d212c0aa6
miao-codes <yunjiccc@gmail.com>
2023-05-29 18:56:02 +0900

    20230529 18:56

--- contracts/dataTypes/BytesExample.sol
+++ contracts/dataTypes/BytesExample.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
 contract BytesExample {
-    //byte는 bytes1 과 같은 표현이며 1개의 byte를 가진 array입니다.
-    //고정바이트배열 : bytes32 와 같이 bytesN 형태를 사용할 수 있는데 1에서 32까지 가능. 컨트랙트간 이동이 가능하려면 고정바이트배열 사용하기
-    //동적(가변)바이트배열: 숫자 없이 'bytes' 로 사용하며 byte[] 로 볼 수 있어 솔리디티의 배열 멤버인 push,pop, length 사용 가능
-    //고정 바이트가 동적 바이트 대비 가스비 소모 적음
+    bytes1[] expensiveByte; //(x)
+    bytes cheaperByte2; //(o)
+
+    bytes32 bytes32Text;
 
     bytes single = "0x1";
     bytes a = "hello ";
@@ -16,7 +16,39 @@ contract BytesExample {
         "Lorem Ipsum is simply dummy text of the printing and typesetting";
     string c =
         "Lorem Ipsum is simply dummy text of the printing and typesetting";
-    bytes addr = "0xA0c68C638235ee32657e8f720a23ceC1bFc77C77";
+    bytes addr = "0xA0c68C";
+
+    function stringToBytes(
+        string calldata text
+    ) public pure returns (bytes memory) {
+        bytes memory byteText = bytes(text);
+        return byteText;
+    }
+
+    function byteToString(
+        bytes calldata code
+    ) public pure returns (string memory) {
+        string memory text = string(code);
+        return text;
+    }
+
+    function getLength() public view returns (uint) {
+        return bytes32Text.length;
+    }
+
+    function getLength2(bytes calldata text) public pure returns (uint) {
+        return text.length;
+    }
+
+    function compare(
+        string calldata s1,
+        string calldata s2
+    ) public pure returns (bool) {
+        // bool result= s1==s2;
+        bool result = keccak256(abi.encodePacked(s1)) ==
+            keccak256(abi.encodePacked(s2));
+        return result;
+    }
 
     // string은 가변인데 이를 고정바이트배열로 변경하는 함수
     // 어떠한 contract가 다른 contract를 string으로 반환할 수 없거나, contract가 string을 사용하고 있어 다른 contract를 호출 할 수 없는 때
@@ -25,7 +57,6 @@ contract BytesExample {
         string memory input
     ) public view returns (bytes32) {
         bytes32 changer = bytes32(abi.encodePacked(input));
-
         return changer;
     }
 
